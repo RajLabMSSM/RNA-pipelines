25d93c0515440e8554955028e79eebacbb573f86
Jack Humphrey <jack@jacks-mbp-3.home>
2020-03-23 17:56:30 -0400

    started CQN scripts

---
+++ cqn/get_gene_length_GC.R
@@ -0,0 +1,13 @@
+library("biomaRt")
+
+ensembl <- useMart("ensembl")
+ensembl <- useDataset("hsapiens_gene_ensembl",mart=ensembl)
+
+df <- getBM(attributes = c("ensembl_gene_id", "start_position", "end_position", "percentage_gene_gc_content"), mart = ensembl)
+
+df$length <- as.numeric(df$end_position) - as.numeric(df$start_position)
+
+library("readr")
+
+write_tsv(df, path = "homo_sapiens_ensembl_gc_length.tsv.gz")
+
